6ef61d6afd88be8b15c76a127df870be7d25c1cc
sondresylte <sondre.sylte@student.uib.no>
2023-03-02 12:24:46 +0100

    Laget ferdig FAQ siden.

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team";
 import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
+import FAQ from "./scenes/faq";
 // import Bar from "./scenes/bar";
 import Form from "./scenes/form";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
-// import FAQ from "./scenes/faq";
 // import Geography from "./scenes/geography";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
@@ -36,10 +36,10 @@ function App() {
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
+              <Route path="/faq" element={<FAQ />} />
               {/*<Route path="/bar" element={<Bar />} />
               <Route path="/line" element={<Line />} />
               <Route path="/pie" element={<Pie />} />
-              <Route path="/faq" element={<FAQ />} />
               <Route path="/geography" element={<Geography />} />*/}
               <Route path="/calendar" element={<Calendar />} /> 
             </Routes>
--- src/scenes/faq/index.jsx
+++ src/scenes/faq/index.jsx
@@ -1,13 +1,99 @@
-import {Box, useTheme, Typography} from "@mui/material";
+import React from "react";
+import { Box, useTheme, Typography } from "@mui/material";
 import Header from "../../components/Header";
-import Accordion from "@mui/material";
+import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import {tokens} from "../../theme";
+import { tokens } from "../../theme";
 
 const FAQ = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
 
-}
+  return (
+    <Box m="20px">
+      <Header title="FAQ" subtitle="Frequently Asked Questions Page" />
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
+            <Typography color={colors.greenAccent[500]} variant = "h5">
+                Hva er poenget med denne siden?
+            </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+            <Typography>
+                Vet ikke enda.
+            </Typography>
+        </AccordionDetails>
+        </Accordion>  
+
+        <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
+            <Typography color={colors.greenAccent[500]} variant = "h5">
+                Hvordan laget du denne siden?
+            </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+            <Typography>
+                React!! og Javasript!
+            </Typography>
+        </AccordionDetails>
+        </Accordion> 
+
+        <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
+            <Typography color={colors.greenAccent[500]} variant = "h5">
+                Flere spm...
+            </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+            <Typography>
+                Det er ikke et spørsmål.
+            </Typography>
+        </AccordionDetails>
+        </Accordion> 
+
+        <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
+            <Typography color={colors.greenAccent[500]} variant = "h5">
+                An important question
+            </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+            <Typography>
+                Dette er en viktig tekst.
+            </Typography>
+        </AccordionDetails>
+        </Accordion> 
+
+        <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
+            <Typography color={colors.greenAccent[500]} variant = "h5">
+                An important question
+            </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+            <Typography>
+                Dette er en viktig tekst.
+            </Typography>
+        </AccordionDetails>
+        </Accordion> 
+
+        <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
+            <Typography color={colors.greenAccent[500]} variant = "h5">
+                An important question
+            </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+            <Typography>
+                Dette er en viktig tekst.
+            </Typography>
+        </AccordionDetails>
+        </Accordion>     
+    
+    </Box>
+  );
+};
 
 export default FAQ;
