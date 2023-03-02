ce3e941de373e5b3fc0aba1778fdf6e4320f7efc
sondresylte <sondre.sylte@student.uib.no>
2023-03-02 11:14:40 +0100

    test

--- src/scenes/calendar/index.jsx
+++ src/scenes/calendar/index.jsx
@@ -2,11 +2,13 @@
 
 import React, { useState } from "react";
 import FullCalendar from "@fullcalendar/react";
-import {formatDate}  from "@fullcalendar/react";
+import formatDate  from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import listPlugin from "@fullcalendar/list";
+
+//test
 import {
   Box,
   List,
